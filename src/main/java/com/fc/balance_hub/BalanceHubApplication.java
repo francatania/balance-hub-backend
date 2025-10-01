0a29a1a1b61db60fc96c3ff486799db3f95be45b
francatania <francocatania93@gmail.com>
2025-09-30 22:03:53 -0300

    spring initialized

---
+++ src/main/java/com/fc/balance_hub/BalanceHubApplication.java
@@ -0,0 +1,13 @@
+package com.fc.balance_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceHubApplication.class, args);
+	}
+
+}
